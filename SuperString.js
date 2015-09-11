f2a044b66fe8e28406839440765d7a570ffc9216
Nguyen Van Phong <katula@Nguyens-MacBook-Pro-2.local>
2015-09-12 01:38:06 +0700

    homework

--- SuperString.js
+++ SuperString.js
@@ -1,1 +1,48 @@
-module
+"use strick";
+
+module.exports.invert = function(data){
+	var itemIvert = "";
+	for (i in data) {
+		itemIvert = data[i] + itemIvert;
+	}
+
+	console.log(itemIvert);
+};
+
+module.exports.camelCase = function(data){
+	var itemcamelCase = "";
+	var check = true;
+
+	for (i in data) {
+
+		if(check && data[i] != " ") {
+			itemcamelCase =  itemcamelCase + data[i].toUpperCase();
+			check = false;
+		} else {
+			itemcamelCase = itemcamelCase + data[i];
+		}
+
+		if(data[i] == " "){
+			check = true;
+		}
+	}
+	console.log(itemcamelCase);
+};
+
+module.exports.longestWord = function(data){
+	var itemWord = "";
+	var itemLongestWord = "";
+	data = data + " ";
+	for (i in data ) {
+		//console.log(i);
+		if(data[i] == " "){
+			if(itemWord.length >= itemLongestWord.length){
+				itemLongestWord = itemWord;
+			}
+			itemWord = "";
+		}else {
+			itemWord = itemWord + data[i];			
+		}
+	}
+	console.log(itemLongestWord);
+};
---
+++ functionTest.js
@@ -0,0 +1,9 @@
+
+
+var superString = require("./SuperString");
+
+superString.invert(" ab cd ef ghik ");
+
+superString.camelCase("in hoa ki tu dau tien");
+
+superString.longestWord(" 123 1234 123456 121 8920293");
